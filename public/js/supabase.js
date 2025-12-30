e2f738c857d466c757b5f4be6f389f071991e5d8
fernandoneto1210-sys <fernandoneto1210@gmail.com>
2025-12-30 14:57:34 -0300

    Update supabase.js

--- public/js/supabase.js
+++ public/js/supabase.js
@@ -4,5 +4,3 @@ export const supabase = createClient(
   "https://pciiucayiytywgaqllbm.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjaWl1Y2F5aXl0eXdnYXFsbGJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0OTUxMzksImV4cCI6MjA4MjA3MTEzOX0.jcwZfItZWa_YxTf27658m78sC1Vu482N2cddNVOlH4Y"
 );
-
-
